4de36ef09f7ed64e6c048e6eb4c84bb3921ab5db
yzu1122020 <147011382+yzu1122020@users.noreply.github.com>
2023-10-05 09:59:33 +0800

    zz
    hw

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
+/ ***  
+  *  hw 20231005
+  *  @author:1122020
+  ***  name:鍾佑澤/
 #include <iostream>
 using namespace std;
 int Fib(int n)
 {
-    return 1;
+    if Fib(1==n)
+      return 1;
+    else
+      return Fib(n-1)+n; 
 }
 int main()
 {
-   cout<<Fib(5)<<endl;
-   return 1;
+    cout<<Fib(1)<<endl;
+    cout<<Fib(2)<<endl;
+    cout<<Fib(3)<<endl;
+    cout<<Fib(4)<<endl;
+    cout<<Fib(5)<<endl;
+   return 0;
 }
